74cff90796cd682ec16beb80eb730ef9c2338ab0
Fernando Souza <fernando.bentomouradesouza@gmail.com>
2022-11-05 23:30:58 -0300

    Level 10 Completed

---
+++ 10 - BuyingInventory/BuyingInventory/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("The following itens are available:");
+Console.WriteLine("1 - Rope");
+Console.WriteLine("2 - Torches");
+Console.WriteLine("3 - Climbing Equipment");
+Console.WriteLine("4 - Clean Water");
+Console.WriteLine("5 - Machete");
+Console.WriteLine("6 - Canoe");
+Console.WriteLine("7 - Food Supplies");
+Console.Write("What numeber do you want to see the price of? ");
+int itemNumber = Convert.ToInt32(Console.ReadLine());
+
+string item = itemNumber switch
+{
+    1 => "Rope",
+    2 => "Torches",
+    3 => "Climbing Equipment",
+    4 => "Clean Water",
+    5 => "Machete",
+    6 => "Canoe",
+    7 => "Food",
+    _ => "Invalid Item"
+};
+
+int price = item switch
+{
+    "Rope" => 10,
+    "Torches" => 15,
+    "Climbing Equipment" => 25,
+    "Clean Water" => 1,
+    "Machete" => 20,
+    "Canoe" => 200,
+    "Food" => 1,
+    "Invalid Item" => 0
+};
+
+Console.WriteLine($"{item} costs {price} gold.");
