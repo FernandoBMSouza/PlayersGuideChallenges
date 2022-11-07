11193ef52d1c38f959aa1618f7b58b63d790a986
Fernando Souza <fernando.bentomouradesouza@gmail.com>
2022-11-07 05:32:38 -0300

    Level 11 completed

---
+++ 11 - TheMagicCannon/TheMagicCannon/Program.cs
@@ -0,0 +1,25 @@
+﻿for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine($"{i}: Eletric and Fire");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"{i}: Eletric");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{i}: Fire");
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine($"{i}: Normal");
+    }
+}
+
+Console.ForegroundColor = ConsoleColor.White;
---
+++ 11 - ThePrototype/ThePrototype/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Clear();
+
+Console.WriteLine("---------------------");
+Console.WriteLine("        PILOT        ");
+Console.WriteLine("---------------------");
+
+int number;
+
+do
+{
+    Console.Write("Enter a number between 0 and 100: ");
+    number = Convert.ToInt32(Console.ReadLine());
+
+} while (number < 0 || number > 100);
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White;
+Console.Clear();
+
+Console.WriteLine("---------------------");
+Console.WriteLine("        HUNTER       ");
+Console.WriteLine("---------------------");
+
+int answer;
+
+for(;;)
+{
+    Console.Write("Guess the number: ");
+    try
+    {
+        answer = Convert.ToInt32(Console.ReadLine());
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Enter a valid number");
+        continue;
+    }
+
+    if(answer == number)
+    {
+        break;
+    }
+    else if(answer > number)
+    {
+        Console.WriteLine($"No, the number is smaller then {answer}.");
+        continue;
+    }
+    else
+    {
+        Console.WriteLine($"No, the number is greater then {answer}.");
+
+    }
+}
+
+Console.WriteLine($"You found the number! It's {answer}.");
