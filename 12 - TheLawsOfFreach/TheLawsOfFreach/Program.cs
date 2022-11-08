7c332497a07291d07548a25c2ed7c8db9d273057
Fernando Souza <fernando.bentomouradesouza@gmail.com>
2022-11-08 00:18:12 -0300

    Level 12 completed

---
+++ 12 - TheLawsOfFreach/TheLawsOfFreach/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = new[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+int minimumValue = int.MaxValue;
+int total = 0;
+foreach (int item in array)
+    if(item < minimumValue) minimumValue = item;
+
+Console.WriteLine("Minimum: " + minimumValue);
+
+foreach (int value in array)
+    total += value;
+
+float avarage = (float)total / array.Length;
+Console.WriteLine($"Avarage: {avarage:#.##}");
---
+++ 12 - TheReplicatorOfDTo/TheReplicatorOfDTo/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Enter five values (,): ");
+string answer = Console.ReadLine();
+
+string[] texValues = answer.Split(',');
+int[] oldArray = new int[texValues.Length];
+
+for (int i = 0; i < texValues.Length; i++)
+{
+    oldArray[i] = Convert.ToInt32(texValues[i]);
+}
+
+int[] newArray = new int[oldArray.Length];
+
+for (int i = 0; i < newArray.Length; i++)
+{
+    newArray[i] = oldArray[i];
+}
+
+Console.WriteLine("\nOldArray: \n");
+for (int i = 0; i < oldArray.Length; i++)
+{
+    Console.WriteLine($"\tIndex({i}): {oldArray[i]}");
+}
+
+Console.WriteLine("\nNewArray: \n");
+for (int i = 0; i < newArray.Length; i++)
+{
+    Console.WriteLine($"\tIndex({i}): {newArray[i]}");
+}
