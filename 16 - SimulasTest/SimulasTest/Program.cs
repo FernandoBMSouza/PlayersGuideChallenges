96b1a955fe3911dc78454fb573fa05ce4b49c263
Fernando Souza <fernando.bentomouradesouza@gmail.com>
2022-11-12 14:06:56 -0300

    Level 16 completed

---
+++ 16 - SimulasTest/SimulasTest/Program.cs
@@ -0,0 +1,23 @@
+﻿ChestState currentChestState = ChestState.Locked;
+
+while (true)
+{
+    Console.Write($"The chest is {currentChestState}. What do you want to do? ");
+    string answer = Console.ReadLine().ToLower();
+
+    switch (currentChestState)
+    {
+        case ChestState.Open:
+            if (answer == "close") currentChestState = ChestState.Closed;
+            break;
+        case ChestState.Closed:
+            if (answer == "lock") currentChestState = ChestState.Locked;
+            else if (answer == "open") currentChestState = ChestState.Open;
+            break;
+        case ChestState.Locked:
+            if (answer == "unlock") currentChestState = ChestState.Closed;
+            break;
+    }
+}
+
+enum ChestState { Open, Closed, Locked };
