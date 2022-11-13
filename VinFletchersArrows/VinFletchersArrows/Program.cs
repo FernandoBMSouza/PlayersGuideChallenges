1b6a9253d51c5049a21762b340eb3ea8ebf88eb1
Fernando Souza <fernando.bentomouradesouza@gmail.com>
2022-11-13 19:44:30 -0300

    Level 18 COMPLETED

---
+++ VinFletchersArrows/VinFletchersArrows/Program.cs
@@ -0,0 +1,92 @@
+﻿Arrow arrow = GetArrow();
+Console.WriteLine("Price: " + arrow.GetCoast() + " gold.");
+Console.ReadKey();
+
+// -------- METHODS ---------
+Arrow GetArrow()
+{
+    Arrowhead head = GetArrowHead();
+    Fletching fletching = GetFletching();
+    float length = GetLengh();
+
+    return new(head, fletching, length);
+}
+float GetLengh()
+{
+    float length;
+
+    do
+    {
+        Console.Write("Enter length: ");
+        length = Convert.ToSingle(Console.ReadLine());
+    } while (length > 100 || length < 60);
+
+    return length;
+
+}
+Fletching GetFletching()
+{
+    Console.Write("Enter fletching type: ");
+    string input = Console.ReadLine().ToLower();
+
+    return input switch
+    {
+        "plastic" => Fletching.Plastic,
+        "turkey feathers" => Fletching.TurkeyFeathers,
+        "goose feathers" => Fletching.GooseFeathers,
+    };
+}
+Arrowhead GetArrowHead()
+{
+    Console.Write("Enter arrowhead: ");
+    string input = Console.ReadLine().ToLower();
+
+    return input switch
+    {
+        "steel" => Arrowhead.Steel,
+        "wood" => Arrowhead.Wood,
+        "obsidian" => Arrowhead.Obsidian,
+    };
+}
+
+// -------- CLASSES ---------
+class Arrow
+{
+    Arrowhead head;
+    Fletching fletching;
+    float length;
+
+    public Arrow(Arrowhead head, Fletching fletching, float length)
+    {
+        this.head = head;
+        this.fletching = fletching;
+        this.length = length;
+    }
+
+    public float GetCoast()
+    {
+        (float headCost, float fletchingCost, float lengthCost) cost;
+
+        cost.headCost = this.head switch
+        {
+            Arrowhead.Steel => 10,
+            Arrowhead.Wood => 3,
+            Arrowhead.Obsidian => 5
+        };
+        
+        cost.fletchingCost = this.fletching switch
+        {
+            Fletching.Plastic => 10,
+            Fletching.TurkeyFeathers => 5,
+            Fletching.GooseFeathers => 3
+        };
+
+        cost.lengthCost = this.length * 0.05f;
+
+        return cost.headCost + cost.fletchingCost + cost.lengthCost;
+    }
+}
+
+// -------- ENUMERATORS ---------
+enum Arrowhead { Steel, Wood, Obsidian }
+enum Fletching { Plastic, TurkeyFeathers, GooseFeathers}
