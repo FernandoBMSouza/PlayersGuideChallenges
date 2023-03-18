d791b3eee521b5776f708b4c69a38f03f546a794
Fernando Souza <fernando.bentomouradesouza@gmail.com>
2023-03-18 19:26:48 -0300

    LEVEL 25

---
+++ 25 - PackingInventory/PackingInventory/Pack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackingInventory
+{
+    internal class Pack
+    {
+        private InventoryItem[] _itens;
+        public int Total { get; }
+        public float MaxWeight { get; }
+        public float MaxVolume { get; }
+
+        public int CurrentTotal { get; private set; }
+        public float CurrentWeight { get; private set; }
+        public float CurrentVolume { get; private set; }
+        public Pack(int total, float maxWeight, float maxVolume)
+        {
+            Total = total;
+            MaxWeight = maxWeight;
+            MaxVolume = maxVolume;
+
+            _itens = new InventoryItem[total];
+        }
+
+        public bool Add(InventoryItem item)
+        {
+            if (CurrentTotal >= Total || CurrentWeight >= MaxWeight || CurrentVolume >= MaxVolume) 
+            {
+                Console.WriteLine("Could not add this to the pack.");
+                return false;
+            }
+            
+            _itens[CurrentTotal] = item;
+            CurrentTotal++;
+            CurrentWeight += item.Weight;
+            CurrentVolume += item.Volume;
+            return true;
+        }
+    }
+}
---
+++ 25 - PackingInventory/PackingInventory/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackingInventory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pack pack = new Pack(10, 10, 10);
+
+            while (true)
+            {
+                Console.WriteLine("1 - Arrow");
+                Console.WriteLine("2 - Bow");
+                Console.WriteLine("3 - Rope");
+                Console.WriteLine("4 - Water");
+                Console.WriteLine("5 - Food");
+                Console.WriteLine("6 - Sword");
+                Console.WriteLine("_ - Finish");
+                Console.Write("Choose an item to add: ");
+
+
+                switch (int.Parse(Console.ReadLine()))
+                {
+                    case 1: pack.Add(new Arrow()); break;
+                    case 2: pack.Add(new Bow()); break;
+                    case 3: pack.Add(new Rope()); break;
+                    case 4: pack.Add(new Water()); break;
+                    case 5: pack.Add(new Food()); break;
+                    case 6: pack.Add(new Sword()); break;
+                }
+                Console.WriteLine("=============================");
+            }
+        }
+    }
+}
