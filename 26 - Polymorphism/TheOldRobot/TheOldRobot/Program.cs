79957f017c8b723674a5b1ccff9f46c5c060f529
Fernando Souza <93844918+FernandoBMSouza@users.noreply.github.com>
2023-03-19 12:54:10 -0300

    level 26 completo

---
+++ 26 - Polymorphism/TheOldRobot/TheOldRobot/Program.cs
@@ -0,0 +1,19 @@
+﻿using TheOldRobot;
+
+Robot robot = new Robot();
+
+for (int i = 0; i < robot.Commands.Length; i++)
+{
+    robot.Commands[i] = Console.ReadLine().ToLower() switch
+    {
+        "on" => new OnCommand(),
+        "off" => new OffCommand(),
+        "north" => new NorthCommand(),
+        "south" => new SouthCommand(),
+        "west" => new WestCommand(),
+        "east" => new EastCommand(),
+    };
+}
+Console.WriteLine();
+robot.Run();
+
---
+++ 26 - Polymorphism/TheOldRobot/TheOldRobot/RobotCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheOldRobot
+{
+    public abstract class RobotCommand
+    {
+        public abstract void Run(Robot robot);
+    }
+
+    public class OnCommand : RobotCommand
+    {
+        public override void Run(Robot robot) => robot.IsPowered = true;
+    }
+    public class OffCommand : RobotCommand
+    {
+        public override void Run(Robot robot) => robot.IsPowered = false;
+    }
+
+    public class NorthCommand : RobotCommand
+    {
+        public override void Run(Robot robot)
+        {
+            if (robot.IsPowered) robot.Y++;
+        }
+    }
+    public class SouthCommand : RobotCommand
+    {
+        public override void Run(Robot robot)
+        {
+            if (robot.IsPowered) robot.Y--;
+        }
+    }
+    public class WestCommand : RobotCommand
+    {
+        public override void Run(Robot robot)
+        {
+            if (robot.IsPowered) robot.X--;
+        }
+    }
+    public class EastCommand : RobotCommand
+    {
+        public override void Run(Robot robot)
+        {
+            if (robot.IsPowered) robot.X++;
+        }
+    }
+}
