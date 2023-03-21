a3869cf7d3adea7081353a9e2b565d35b25809c3
Fernando Souza <93844918+FernandoBMSouza@users.noreply.github.com>
2023-03-21 16:53:30 -0300

    level 27

---
+++ 27 - Interfaces/TheOldRobot/TheOldRobot/RobotCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheOldRobot
+{
+    public interface IRobotCommand
+    {
+         void Run(Robot robot);
+    }
+
+    public class OnCommand : IRobotCommand
+    {
+        public void Run(Robot robot) => robot.IsPowered = true;
+    }
+    public class OffCommand : IRobotCommand
+    {
+        public void Run(Robot robot) => robot.IsPowered = false;
+    }
+
+    public class NorthCommand : IRobotCommand
+    {
+        public void Run(Robot robot)
+        {
+            if (robot.IsPowered) robot.Y++;
+        }
+    }
+    public class SouthCommand : IRobotCommand
+    {
+        public void Run(Robot robot)
+        {
+            if (robot.IsPowered) robot.Y--;
+        }
+    }
+    public class WestCommand : IRobotCommand
+    {
+        public void Run(Robot robot)
+        {
+            if (robot.IsPowered) robot.X--;
+        }
+    }
+    public class EastCommand : IRobotCommand
+    {
+        public void Run(Robot robot)
+        {
+            if (robot.IsPowered) robot.X++;
+        }
+    }
+}
