fcffbbbf94f8db13ddd697084076f5ab0f2671a6
Fernando Souza <93844918+FernandoBMSouza@users.noreply.github.com>
2023-03-22 10:30:56 -0300

    level 28

---
+++ 28 - Structs/RoomCoordinates/RoomCoordinates/Coordinate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomCoordinates
+{
+    public struct Coordinate
+    {
+        public int Row { get; }
+        public int Column { get; }
+
+        public Coordinate(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public static bool AreAdjacent(Coordinate a, Coordinate b)
+        {
+            int columnChange = a.Column - b.Column;
+            int rowChange = a.Row - b.Row;
+
+            if (Math.Abs(columnChange) <= 1 && rowChange == 0) return true;
+            if (Math.Abs(rowChange) <= 1 && columnChange == 0) return true;
+
+            return false;
+        }
+    }
+}
---
+++ 28 - Structs/RoomCoordinates/RoomCoordinates/Program.cs
@@ -0,0 +1,13 @@
+﻿using RoomCoordinates;
+
+Coordinate[] coordinates = new Coordinate[6];
+
+coordinates[0] = new Coordinate(1, 1);
+coordinates[1] = new Coordinate(1, 2);
+coordinates[2] = new Coordinate(1, 3);
+coordinates[3] = new Coordinate(2, 1);
+coordinates[4] = new Coordinate(2, 2);
+coordinates[5] = new Coordinate(2, 3);
+
+Console.WriteLine(Coordinate.AreAdjacent(coordinates[0], coordinates[1]));
+Console.WriteLine(Coordinate.AreAdjacent(coordinates[0], coordinates[2]));
